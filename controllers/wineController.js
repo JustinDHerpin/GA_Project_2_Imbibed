f7d64c1ec7cb7332cd4c94d83afe3a3ef00d9168
Justin Herpin <justindherpin@yahoo.com>
2022-01-07 15:35:10 -0800

    All routes working.  Information passing correctly.  CRUD achieved.  Moving on to add some more buttons in and start styling

--- controllers/wineController.js
+++ controllers/wineController.js
@@ -41,6 +41,12 @@ router.get('/:id/edit', (req, res) => {
 // PUT/PATCH Route for Edit/Update:
 
 router.put('/:id', (req, res) => {
+    if(req.body.liked === 'on'){
+        req.body.liked = true
+    }else{
+        req.body.liked = false
+    }
+
     Wine.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedItem) => {
         res.redirect('/wines/' + req.params.id)
         // console.log(err)
@@ -59,6 +65,14 @@ router.post('/', (req, res) => {
             res.redirect('/wines')
         }
         
+    })
+})
+
+// DELETE Route:
+
+router.delete('/:id', (req, res) => {
+    Wine.findByIdAndDelete(req.params.id, (err, deletedWine) => {
+        res.redirect('/wines')
     })
 })
 
