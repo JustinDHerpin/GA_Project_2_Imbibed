d228e59f5d033563de525056d6c31bf673e8772c
Justin Herpin <justindherpin@yahoo.com>
2022-01-15 15:32:46 -0800

    bcrypt added as dependency.  started encrypting via bcrypt.  user is passed to model and password is showing as encrypted.

--- controllers/sessionsController.js
+++ controllers/sessionsController.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 const router = express.Router()
 
@@ -12,7 +14,23 @@ router.get('/register', (req, res) => {
 
 router.post('/register', async (req, res, next) => {
     try {
-        console.log(req.body)
+        if (req.body.password === req.body.verifyPassword) {
+            const desiredUsername = req.body.username
+            const userExists = await User.findOne({ username: desiredUsername })
+            if (userExists) {
+                res.send('Username already taken')
+            } else {
+                const salt = bcrypt.genSaltSync(10)
+                const hashedPassword = bcrypt.hashSync(req.body.password, salt)
+                req.body.password = hashedPassword
+                const createdUser = await User.create(req.body)
+                console.log(createdUser)
+                res.send('check your terminal')
+            }
+        } else {
+            console.log(req.body)
+            res.send('Passwords must match!')
+        }
     } catch (err) {
         next(err)
     }
